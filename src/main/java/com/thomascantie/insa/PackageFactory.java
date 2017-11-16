90f82343c582c96971f6a89e54d6b96971dc8a9b
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-17 00:10:04 +0100

    -- Apply Design Pattern Singleton

--- src/main/java/com/thomascantie/insa/PackageFactory.java
+++ src/main/java/com/thomascantie/insa/PackageFactory.java
@@ -2,7 +2,19 @@ package com.thomascantie.insa;
 
 public class PackageFactory {
 
-	public static Package createPackage(int height, int width, int depth, double weight, String dest) {
+	private static PackageFactory instance;
+
+	private PackageFactory() {
+
+	}
+
+	public static synchronized PackageFactory getInstance() {
+		if (instance == null)
+			instance = new PackageFactory();
+		return instance;
+	}
+
+	public Package createPackage(int height, int width, int depth, double weight, String dest) {
 
 		if ((height <= 229 && width <= 162) || (width <= 229 && height <= 162) && depth <= 25)
 			return new SmallPackage(height, width, depth, weight, Destination.valueOf(dest));
