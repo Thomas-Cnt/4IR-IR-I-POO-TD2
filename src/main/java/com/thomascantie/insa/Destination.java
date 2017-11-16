0fa1b5a98ee5c9ba29520fdb4717566fa0449de7
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-16 22:03:51 +0100

    -- Add DOM/TOM Destination

--- src/main/java/com/thomascantie/insa/Destination.java
+++ src/main/java/com/thomascantie/insa/Destination.java
@@ -1,12 +1,19 @@
 package com.thomascantie.insa;
 
 public enum Destination {
+
 	FR("FR") ,
-	MC("MC");
+	MC("MC") ,
+	DOM_TOM("DOM/TOM") ;
 
 	private String text;
 
 	Destination(final String st) {
 		this.text = st;
+	}
+
+	@Override
+	public String toString() {
+		return this.text;
 	}
 }
