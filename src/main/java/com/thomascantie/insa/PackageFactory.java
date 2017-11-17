c43b96f6b1fd8b79c6a4aad5c1139b93ef5fba54
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-17 08:40:17 +0100

    -- Remove unused parameter

--- src/main/java/com/thomascantie/insa/PackageFactory.java
+++ src/main/java/com/thomascantie/insa/PackageFactory.java
@@ -14,15 +14,15 @@ public class PackageFactory {
 		return instance;
 	}
 
-	public Package createPackage(int height, int width, int depth, double weight, String dest) {
+	public Package createPackage(int height, int width, int depth, double weight) {
 
 		if ((height <= 229 && width <= 162) || (width <= 229 && height <= 162) && depth <= 25)
-			return new SmallPackage(height, width, depth, weight, Destination.valueOf(dest));
+			return new SmallPackage(height, width, depth, weight);
 
 		if (weight <= 1.8)
-			return new MediumPackage(height, width, depth, weight, Destination.valueOf(dest));
+			return new MediumPackage(height, width, depth, weight);
 
-		return new BigPackage(height, width, depth, weight, Destination.valueOf(dest));
+		return new BigPackage(height, width, depth, weight);
 
 	}
 
