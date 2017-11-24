18fd6b0a63a6f1b2cdab4cc9aaac2c14e3ab1844
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-24 11:16:37 +0100

    -- add conditions when a package is small

--- src/main/java/com/thomascantie/insa/PackageFactory.java
+++ src/main/java/com/thomascantie/insa/PackageFactory.java
@@ -16,7 +16,7 @@ public class PackageFactory {
 
 	public Package createPackage(int height, int width, int depth, double weight) {
 
-		if ((height <= 229 && width <= 162) || (width <= 229 && height <= 162) && depth <= 25)
+		if (hasLittleDimensions(height, width, depth))
 			return new SmallPackage(height, width, depth, weight);
 
 		if (weight <= 1.8)
@@ -24,6 +24,18 @@ public class PackageFactory {
 
 		return new BigPackage(height, width, depth, weight);
 
+	}
+
+	private boolean hasLittleDimensions(int h, int w, int d) {
+
+		if (h <= 229 && w <= 162 && d <= 25
+			|| h <= 229 && d <= 162 && w <= 25
+			|| w <= 229 && h <= 162 && d <= 25
+			|| w <= 229 && d <= 162 && h <= 25
+			|| d <= 229 && h <= 162 && w <= 25
+			|| d <= 229 && w <= 162 && h <= 25)
+			return true;
+		return false;
 	}
 
 
